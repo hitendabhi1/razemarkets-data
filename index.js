1d33656829ff2b9f1d7cd2c060fc17cda36f17e3
Hiten Dabhi <hitendabhi@Hitens-MacBook-Pro.local>
2024-11-25 10:27:29 +0000

    changes

--- index.js
+++ index.js
@@ -8,162 +8,184 @@ app.use(express.json());
 
 const port = 3002;
 
-app.get("/getCoupon", async (req, res) => {
-  const consumerKey = "ck_2a0a8dbfc93b28c02e37470fda71d0f816d99c5d";
-  const consumerSecret = "cs_473149ccb5b9382ef46730d8e2c53a762f4b57ed";
-  const siteUrl = "https://join.optimal-traders.com/";
+const splitPrices = {
+  AMZN: 10,
+  TSLA: 10,
+  AAPL: 10,
+  GOOGL: 10,
+  NVDA: 10,
+  USDJPY: 100,
+  EURUSD: 10000,
+  GBPUSD: 10000,
+  USDCAD: 10000,
+  EURGBP: 10000,
+  XAUUSD: 10,
+  XAGUSD: 100,
+  WTIUSD: 100,
+  BRNUSD: 100,
+  NGCUSD: 4,
+  SPXUSD: 10,
+  DJIUSD: 10,
+  NDXUSD: 10,
+  FTSGBP: 10,
+  DAXEUR: 10,
+  BTCUSD: 100,
+  ETHUSD: 100,
+  XRPUSD: 10000,
+  LTCUSD: 10000,
+  ADAUSD: 10000,
+};
+// Middleware to parse incoming JSON requests
+app.use(express.json());
 
-  // Define prizes and probabilities
-  const prizes = [
-    // Tier 1
-    {
-      name: "30% off 2-Step Challenge",
-      probability: 1,
-      discountType: "percent",
-      discountAmount: 30,
-      productId: 846,
-    },
-    // Tier 2
-    {
-      name: "25% off 2-Step Challenge",
-      probability: 2,
-      discountType: "percent",
-      discountAmount: 25,
-      productId: 846,
-    },
-    // Tier 3
-    {
-      name: "20% off 2-Step Challenge",
-      probability: 7,
-      discountType: "percent",
-      discountAmount: 20,
-      productId: 846,
-    },
-    // Tier 4
-    {
-      name: "15% off 2-Step Challenge",
-      probability: 10,
-      discountType: "percent",
-      discountAmount: 15,
-      productId: 846,
-    },
-    {
-      name: "15% off 1-Step Algo",
-      probability: 10,
-      discountType: "percent",
-      discountAmount: 15,
-      productId: 853,
-    },
-    // Tier 5
-    {
-      name: "15% off 1-Step Standard",
-      probability: 20,
-      discountType: "percent",
-      discountAmount: 15,
-      productId: 830,
-    },
-    {
-      name: "12.5% off 1-Step Algo",
-      probability: 20,
-      discountType: "percent",
-      discountAmount: 12.5,
-      productId: 853,
-    },
-    // Tier 6
-    {
-      name: "10% off 1-Step Algo",
-      probability: 30,
-      discountType: "percent",
-      discountAmount: 10,
-      productId: 853,
-    },
-    {
-      name: "10% off 1-Step Standard",
-      probability: 30,
-      discountType: "percent",
-      discountAmount: 10,
-      productId: 830,
-    },
-  ];
-
-  // Helper function to select a prize based on probability
-  const selectPrize = () => {
-    const totalProbability = prizes.reduce(
-      (sum, prize) => sum + prize.probability,
-      0
-    );
-
-    const randomMath = Math.random();
-    const random = randomMath * totalProbability;
-    let cumulative = 0;
-    for (const prize of prizes) {
-      cumulative += prize.probability;
-
-      if (random < cumulative) return prize;
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
     }
-  };
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return { error: error.message };
+  }
+};
 
-  const selectedPrize = selectPrize();
+// Function to format the data
+const formatData = (data) => {
+  let dataObject = {};
 
-  // Generate a unique coupon code
-  const generateUniqueCode = () =>
-    `COUPON_${Date.now()}_${Math.random()
-      .toString(36)
-      .substr(2, 5)
-      .toUpperCase()}`;
-  const couponCode = generateUniqueCode();
+  data.forEach((item) => {
+    const bid = item.bid;
+    const ask = item.ask;
+    const splitDifference = (ask - bid) * splitPrices[item.symbol];
+    const percentDifference = (splitDifference / bid) * 100;
+    dataObject = {
+      ...dataObject,
+      [item.symbol]: {
+        bid: bid,
+        ask: ask,
+        split: splitDifference,
+        percentDifference: percentDifference.toFixed(2), // rounding to 2 decimal places
+      },
+    };
+  });
 
-  // Coupon data payload
-  const couponData = {
-    code: couponCode,
-    discount_type: selectedPrize.discountType,
-    amount: selectedPrize.discountAmount?.toString() || 0, // If no discount amount, it's a free product
-    product_ids: [selectedPrize.productId],
-    individual_use: true,
-    usage_limit: 1,
-    usage_limit_per_user: 1,
-    name: selectedPrize.name,
-    expiry_date: "2024-12-31",
-  };
+  return dataObject;
+};
 
-  // res.status(200).json({
-  //   couponData,
-  // });
+// Helper function to calculate percentage change
+function calculatePercentageChange(openPrice, closePrice) {
+  return 100 * ((closePrice - openPrice) / openPrice);
+}
+
+let previousPrices = {};
+
+// app.get("/spreads", async (req, res) => {
+//   await fetch(
+//     "http://212.117.171.68:5051/SymbolParamsMany?id=12a9d189-4513-4438-999f-86429849fe5b&symbols=AMZN&symbols=TSLA&symbols=AAPL&symbols=GOOGL&symbols=NVDA&symbols=USDJPY&symbols=EURUSD&symbols=GBPUSD&symbols=USDCAD&symbols=EURGBP&symbols=XAUUSD&symbols=XAGUSD&symbols=WTIUSD&symbols=SPXUSD&symbols=DJIUSD&symbols=NDXUSD&symbols=FTSGBP&symbols=DAXEUR&symbols=BTCUSD&symbols=ETHUSD&symbols=XRPUSD&symbols=LTCUSD&symbols=ADAUSD&symbols=BRNUSD&symbols=NGCUSDlimit=10000"
+//   )
+//     .then((response) => response.text())
+//     .then((result) => {
+//       // console.log(result);
+//       result = JSON.parse(result);
+
+//       const symbols = [];
+//       const resultMap = result.map((item) => {
+//         return {
+//           [item.symbol]: item.symbolInfo.digits,
+//         };
+//       });
+//       res.json(resultMap);
+//       // result.forEach((item) => {
+//       //   console.log(item);
+//       // });
+//     })
+//     .catch((error) => console.error(error));
+//   // try {
+//   // } catch (error) {
+//   //   // res.send('error')
+//   // }
+// });
+
+app.get("/prices", async (req, res) => {
+  const url =
+    "http://212.117.171.68:5051/Connect?user=100476&password=T%2BDmV0Kk&host=57.128.140.226&port=443";
 
   try {
-    // Send request to WooCommerce REST API
-    const response = await fetch(`${siteUrl}/wp-json/wc/v3/coupons`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Basic ${Buffer.from(
-          `${consumerKey}:${consumerSecret}`
-        ).toString("base64")}`,
-      },
-      body: JSON.stringify(couponData),
+    const response = await fetch(url, {
+      method: "GET",
+      headers: { "Content-Type": "text/plain" },
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+
+    const textData = await response.text();
+
+    const pricesData = await fetchData(
+      `http://212.117.171.68:5051/GetQuoteMany?id=${textData}&symbols=AMZN&symbols=TSLA&symbols=AAPL&symbols=GOOGL&symbols=NVDA&symbols=USDJPY&symbols=EURUSD&symbols=GBPUSD&symbols=USDCAD&symbols=EURGBP&symbols=XAUUSD&symbols=XAGUSD&symbols=WTIUSD&symbols=SPXUSD&symbols=DJIUSD&symbols=NDXUSD&symbols=FTSGBP&symbols=DAXEUR&symbols=BTCUSD&symbols=ETHUSD&symbols=XRPUSD&symbols=LTCUSD&symbols=ADAUSD&symbols=BRNUSD&symbols=NGCUSD`
+    );
+
+    const openCloseData = await fetchData(
+      `http://212.117.171.68:5051/PriceHistoryTodayMany?id=${textData}&symbols=AMZN&symbols=TSLA&symbols=AAPL&symbols=GOOGL&symbols=NVDA&symbols=USDJPY&symbols=EURUSD&symbols=GBPUSD&symbols=USDCAD&symbols=EURGBP&symbols=XAUUSD&symbols=XAGUSD&symbols=WTIUSD&symbols=SPXUSD&symbols=DJIUSD&symbols=NDXUSD&symbols=FTSGBP&symbols=DAXEUR&symbols=BTCUSD&symbols=ETHUSD&symbols=XRPUSD&symbols=LTCUSD&symbols=ADAUSD&symbols=BRNUSD&symbols=NGCUSD&timeFrame=1440`
+    );
+
+    let dataObject = formatData(pricesData);
+
+    // Add dailyChange to the dataObject
+    openCloseData.forEach(({ symbol, bars }) => {
+      const lastBar = bars[bars.length - 1];
+      const dailyChange = calculatePercentageChange(
+        lastBar.openPrice,
+        lastBar.closePrice
+      );
+      if (dataObject[symbol]) {
+        dataObject[symbol].dailyChange = dailyChange.toFixed(2) + "%";
+      }
     });
 
-    // Handle response
-    if (response.ok) {
-      const responseData = await response.json();
-      res.status(200).json({
-        success: true,
-        prize: selectedPrize.name,
-        coupon: responseData,
-      });
-    } else {
-      const errorData = await response.json();
-      res.status(400).json({
-        success: false,
-        error: errorData,
-      });
+    // Add price direction indicator
+    for (const symbol in dataObject) {
+      const currentPrice = dataObject[symbol].bid;
+      const dailyChange = dataObject[symbol].dailyChange;
+
+      // Determine price direction (up, down, unchanged)
+      if (previousPrices[symbol] !== undefined) {
+        if (currentPrice > previousPrices[symbol]) {
+          dataObject[symbol].biddirection = "up";
+        } else if (currentPrice < previousPrices[symbol]) {
+          dataObject[symbol].biddirection = "down";
+        } else {
+          dataObject[symbol].biddirection = "unchanged";
+        }
+      } else {
+        dataObject[symbol].biddirection = "new"; // First-time fetch, no previous data
+      }
+      // // Determine price direction (up, down, unchanged)
+      // if (previousPrices[symbol] !== undefined) {
+      //   if (dailyChange > previousPrices[symbol]) {
+      //     dataObject[symbol].percentagedirection = "up";
+      //   } else if (dailyChange < previousPrices[symbol]) {
+      //     dataObject[symbol].percentagedirection = "down";
+      //   } else {
+      //     dataObject[symbol].percentagedirection = "unchanged";
+      //   }
+      // } else {
+      //   dataObject[symbol].percentagedirection = "new"; // First-time fetch, no previous data
+      // }
+      dataObject[symbol].percentagedirection = dailyChange.includes("-")
+        ? "down"
+        : "up";
+
+      // Store the current price for the next comparison
+      previousPrices[symbol] = currentPrice;
     }
+
+    res.json(dataObject);
   } catch (error) {
-    console.error("Error creating coupon:", error);
-    res.status(500).json({
-      success: false,
-      message: "Internal server error while creating coupon.",
-    });
+    console.error("There was a problem with the fetch operation:", error);
+    res.status(500).json({ error: "Failed to fetch prices data" });
   }
 });
 
